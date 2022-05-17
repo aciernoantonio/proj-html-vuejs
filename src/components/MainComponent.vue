b0723b0dcef907f622f981101b46d3d2191800ad
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-17 10:53:54 +0200

    add excellence section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -104,12 +104,127 @@
         </div>
         <!-- /.company -->
 
+        <div class="services">
+
+            <div class="container excellence">
+
+                <div class="uptitle">OUR BUSINESS AREAS</div>
+                <div class="company mt-4 text-white">
+                    Excellence in <span class="marked_dark text-white rounded-3">Services</span>
+                </div>
+
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-9">
+                        <div class="subtext text-muted my-4 text-start">
+                            We are leaders in providing consultancy services with a set of cutting-edge technologies and a team of
+                            experienced and renowned professionals. These are some options taht you can hire.
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">SEE ALL</a>
+                    </div>
+                </div>
+
+            
+
+            </div>
+            <!-- /.excellence -->
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-4">
+                        <div class="card bg-dark text-white p-3 border-0">
+                            <div class="card-body d-flex flex-column align-items-start">
+                                <div class="icon mb-3"><font-awesome-icon icon="fa-solid fa-network-wired" /></div>
+                                <h5 class="card-title">Audit & Assurcance</h5>
+                                <h6 class="card-subtitle my-2 text-muted mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                                <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="card bg-dark text-white p-3 border-0">
+                            <div class="card-body d-flex flex-column align-items-start">
+                                <div class="icon mb-3"><font-awesome-icon icon="fa-solid fa-briefcase" /></div>
+                                <h5 class="card-title">Financial Advisory</h5>
+                                <h6 class="card-subtitle my-2 text-muted mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                                <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="card bg-dark text-white p-3 border-0">
+                            <div class="card-body d-flex flex-column align-items-start">
+                                <div class="icon mb-3"><font-awesome-icon icon="fa-solid fa-chart-simple" /></div>
+                                <h5 class="card-title">Analytics and M&A</h5>
+                                <h6 class="card-subtitle my-2 text-muted mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                                <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4 mt-4">
+                        <div class="card bg-dark text-white p-3 border-0">
+                            <div class="card-body d-flex flex-column align-items-start">
+                                <div class="icon mb-3"><font-awesome-icon icon="fa-solid fa-plane" /></div>
+                                <h5 class="card-title">Middle Marketing</h5>
+                                <h6 class="card-subtitle my-2 text-muted mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                                <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4 mt-4">
+                        <div class="card bg-dark text-white p-3 border-0">
+                            <div class="card-body d-flex flex-column align-items-start">
+                                <div class="icon mb-3"><font-awesome-icon icon="fa-solid fa-globe" /></div>
+                                <h5 class="card-title">Legal Consulting</h5>
+                                <h6 class="card-subtitle my-2 text-muted mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                                <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-4 mt-4">
+                        <div class="card bg-dark text-white p-3 border-0">
+                            <div class="card-body d-flex flex-column align-items-start">
+                                <div class="icon mb-3"><font-awesome-icon icon="fa-solid fa-inbox" /></div>
+                                <h5 class="card-title">Regulatory Risk</h5>
+                                <h6 class="card-subtitle my-2 text-muted mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                                <a class="btn btn-empty btn-primary py-2 px-3" href="#" role="button">READ MORE</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            
+
+        </div>
+
+
+
     </main>
 </template>
 
 <style lang="scss" scoped>
 
 main{
+
+    .text-white{
+        color: white;
+    }
+
+    .uptitle{
+            font-size: 12px;
+            color: #0CA6B0;
+            font-weight: 600;
+        }
 
     .icon{
         color: #328486;
@@ -151,7 +266,8 @@ main{
 
     }
 
-    .about{
+    .about,
+    .services{
 
         .network{
             font-size: 12px;
@@ -166,7 +282,6 @@ main{
 
         .marked{
             color: #058283;
-            background-color: #DAECED;
             padding: 0.75rem 1rem;
         }
 
@@ -183,6 +298,46 @@ main{
         }
 
     }
+
+    .services{
+
+        padding: 7rem 0;
+        background-color: black;
+
+        .btn-empty{
+            background-color: transparent;
+            border: 1px solid rgb(1, 165, 165);
+        }
+
+        .marked_dark{
+            background-color: rgba(1, 165, 165, 0.3);
+            padding: 0.75rem 1rem;
+        }
+
+        .container{
+            width: 850px;
+            margin: auto;
+        }
+
+        .card-title{
+            font-weight: 600;
+        }
+
+        .icon{
+            color: rgb(1, 165, 165);
+            font-size: 25px;
+        }
+
+        .card-title{
+            font-size: 18px;
+        }
+
+        .card-subtitle{
+            font-size: 13px;
+        }
+
+    }
+
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faClock, faPhone, faEnvelope, faUserGroup, faChartPie, faGaugeHigh, faEllipsis, faMedal, faLock, faFilePen, faGraduationCap} from '@fortawesome/free-solid-svg-icons'
+import { faClock, faPhone, faEnvelope, faUserGroup, faChartPie, faGaugeHigh, faEllipsis, faMedal, faLock, faFilePen, faGraduationCap, faRocket, faGem, faNetworkWired, faChartSimple, faPlane, faGlobe, faInbox, faBriefcase} from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF, faLinkedinIn, faTwitter} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(
   faClock, faPhone, faEnvelope, faFacebookF, faTwitter, faLinkedinIn, faUserGroup,
   faChartPie, faGaugeHigh, faEllipsis, faMedal, faLock, faFilePen, faGraduationCap,
-  
+  faRocket, faBriefcase, faGem, faNetworkWired, faChartSimple, faPlane, faGlobe, faInbox
   )
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
